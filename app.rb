4e590cf993d02865b00070adf4928014bbd7426e
Fredrick <fredrick.lokidor@student.moringaschool.com>
2022-08-15 12:08:27 +0300

    completed

---
+++ app.rb
@@ -0,0 +1,11 @@
+puts"HELLO WORLD!!"
+puts "Hello World!"
+#puts "Hello world!"
+#puts "Hello world!"
+print "Hello world!"
+print "Hello world!"
+print "Hello world!"
+p [1, 2, 3]
+pp [1, 2, 3]
+pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+print "Pass this test, please."
